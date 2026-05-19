c92dbc4953fbea8bac8dfe4b6dda2b8df957d2f2
Build Frame Digital System Builder <buildframe.digitalsystem@gmail.com>
2026-05-19 11:32:05 +0800

    Update API URL in api-config.js

--- assets/js/api-config.js
+++ assets/js/api-config.js
@@ -1,2 +1,2 @@
 const PINOY_CASEY_API_URL =
-    "https://script.google.com/macros/s/AKfycbyW5k4c9yfjXgM6nYAHwqIDQLtYBOSGozbxjsr4Llz-8te5exvChi804NCMk_Wym-bOzg/exec";
+"https://script.google.com/macros/s/AKfycbxlsNq-rTnXTOOtFbJCHkhxjqOLj9ppM-RAfytvoygoMQyhPNcm-SrDPlxtRh_24472ng/exec";
